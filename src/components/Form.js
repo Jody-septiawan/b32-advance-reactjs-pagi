48ad4eea87ca21b443df2176b5b20440aa866524
Jody-septiawan <jody.septiawan5@gmail.com>
2022-03-29 09:47:51 +0700

    CSS Modules

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,29 +1,31 @@
 // import css modules
+import cssModules from './Form.module.css';
+
+console.log(cssModules);
 
 const styles = {
   form: {
-    margin: "16px 20% 0",
+    margin: '16px 20% 0',
   },
   formGroup: {
-    marginBottom: "16px",
+    marginBottom: '16px',
   },
   formLabel: {
-    marginBottom: "8px",
-    display: "inline-block",
+    marginBottom: '8px',
+    display: 'inline-block',
   },
   formInput: {
-    display: "block",
-    width: "100%",
-    padding: ".375rem .75rem",
-    fontSize: "1rem",
+    display: 'block',
+    width: '100%',
+    padding: '.375rem .75rem',
+    fontSize: '1rem',
     lineHeight: 1.5,
-    color: "#212529",
-    backgroundColor: "#fff",
-    border: "1px solid #ced4da",
-    borderRadius: ".25rem",
+    color: '#212529',
+    backgroundColor: '#fff',
+    border: '1px solid #ced4da',
+    borderRadius: '.25rem',
   },
 };
-
 
 function Form() {
   return (
@@ -39,10 +41,19 @@ function Form() {
             style={styles.formInput}
           />
         </div>
-        {/* code here */}
+        <div className={cssModules.formGroup}>
+          <label htmlFor="email" className={cssModules.formLabel}>
+            Email
+          </label>
+          <input
+            id="email"
+            className={cssModules.formInput}
+            placeholder="Input email"
+          />
+        </div>
       </form>
     </div>
-  )
+  );
 }
 
-export default Form
+export default Form;
