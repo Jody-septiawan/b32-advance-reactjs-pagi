adb4210f714ace97cd5e397505ed33c238e4f93b
Jody-septiawan <jody.septiawan5@gmail.com>
2022-03-29 11:49:54 +0700

    Prop-Types

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,7 @@
-import { Container } from "react-bootstrap";
+import { Container } from 'react-bootstrap';
 
 // import component
+import DetailAbout from '../components/DetailAbout';
 
 function About() {
   return (
@@ -16,7 +17,7 @@ function About() {
         quis nibh gravida, ut hendrerit felis fringilla. Proin at aliquam
         tortor.
       </p>
-      {/* code here */}
+      <DetailAbout title="test" summary="No. 1 Automobile compani in USA" />
     </Container>
   );
 }
